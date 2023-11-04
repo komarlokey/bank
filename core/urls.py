3f45518a3aea3ba637207206151c043d92b763de
komarlokey <komarlokey2@gmail.com>
2023-11-04 16:33:47 +0600

    Add Static

--- core/urls.py
+++ core/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework.permissions import AllowAny
+from django.conf import settings
+from django.conf.urls.static import static
 
 SchemaView = get_schema_view(
     openapi.Info(
@@ -44,3 +46,6 @@ urlpatterns = [
     path('shop/', include("shop.urls")),
     path('docs/', SchemaView.with_ui("swagger", cache_timeout=0), name="docs"),
 ]
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
